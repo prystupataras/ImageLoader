7ed7bea14515cadb7a0e454afc3661a2c12fa87b
Taras <45391115+prystupataras@users.noreply.github.com>
2024-08-30 09:47:38 +0300

    Initial Commit

---
+++ ImageLoader/ImageLoaderApp.swift
@@ -0,0 +1,17 @@
+//
+//  ImageLoaderApp.swift
+//  ImageLoader
+//
+//  Created by Taras Prystupa on 30.08.2024.
+//
+
+import SwiftUI
+
+@main
+struct ImageLoaderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
